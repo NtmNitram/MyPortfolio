044f27fcc2c8680909964663d03ed0b42eb223b8
NtmNitram <ntmbohorquez@gmail.com>
2023-04-26 21:49:59 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './Componentes/encabezado/encabezado.component';
+import { AboutComponent } from './Componentes/about/about.component';
+import { EducationComponent } from './Componentes/education/education.component';
+import { ExperienceComponent } from './Componentes/experience/experience.component';
+import { ProjectsComponent } from './Componentes/projects/projects.component';
+import { SkillsComponent } from './Componentes/skills/skills.component';
+import { ContactComponent } from './Componentes/contact/contact.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AboutComponent,
+    EducationComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
